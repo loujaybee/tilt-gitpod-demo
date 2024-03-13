ca271f9f2d19c0a9b90f8d08b20c877a3d45ecdc
Louis Bichard <louis@gitpod.io>
2024-03-13 13:55:18 +0000

    fix: update demo

--- microservice/main.js
+++ microservice/main.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-    res.send(`Microservice A running on port ${PORT} - DX DAY!`);
+    res.send(`Microservice A running on port ${PORT} - DX DAY!!!!`);
 });
 
 app.listen(PORT, () => {
